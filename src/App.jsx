bff8850d2b87aa92330e01b5f2c9109d995f90e4
Saheb Mohamadi <saheb.put92@gmail.com>
2023-07-22 00:02:43 +0330

    remove note

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,19 @@ function App() {
     setNotes((prevNotes) => [...prevNotes, newNote]);
   };
 
+  const handleDeleteNote = (id) => {
+    // const filteredNotes = notes.filter((n) => n.id !== id);
+    // setNotes(filteredNotes);
+    setNotes((prevNotes) => prevNotes.filter((n) => n.id !== id));
+  };
+
   return (
     <div className="container">
       <div className="note-header">note header</div>
       <div className="note-app">
         <AddNewNote onAddNote={hanldeAddNote} />
         <div className="note-container">
-          <NoteList notes={notes}/>
+          <NoteList notes={notes} onDelete={handleDeleteNote} />
         </div>
       </div>
     </div>
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,8 +1,37 @@
-
-function NoteList({notes}) {
+function NoteList({ notes, onDelete }) {
   return (
-    <div>NoteList</div>
-  )
+    <div className="note-list">
+      {notes.map((note) => (
+        <NoteItem key={note.id} note={note} onDelete={onDelete} />
+      ))}
+    </div>
+  );
 }
 
-export default NoteList
+export default NoteList;
+
+function NoteItem({ note, onDelete }) {
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  return (
+    <div className="note-item">
+      <div className="note-item__header">
+        <div>
+          <p className="title">{note.title}</p>
+          <p className="desc">{note.description}</p>
+        </div>
+        <div className="actions">
+          <button onClick={() => onDelete(note.id)}>❌</button>
+          <input type="checkbox" name="" id="" />
+        </div>
+      </div>
+      <p className="note-item__footer">
+        {new Date(note.createdAt).toLocaleDateString("en-US", options)}
+      </p>
+    </div>
+  );
+}
