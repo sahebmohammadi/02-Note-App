c190d42445e2900dd74efb3009bb34d89bac9c06
Saheb Mohamadi <saheb.put92@gmail.com>
2023-07-21 17:14:01 +0330

    controlled element

--- src/components/AddNewNote.jsx
+++ src/components/AddNewNote.jsx
@@ -1,14 +1,38 @@
+import { useState } from "react";
+
 function AddNewNote() {
+  const [title, setTtile] = useState("");
+  const [description, setDescription] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    const newNote = {
+      title,
+      description,
+      id: Date.now(),
+      completed: false,
+      createdAt: new Date().toISOString(),
+    };
+
+    setTtile("")
+    setDescription("")
+    // update notes !!
   };
 
   return (
     <div className="add-new-note">
       <h2>Add New Note</h2>
       <form className="note-form" onSubmit={handleSubmit}>
-        <input type="text" className="text-field" placeholder="Note title" />
         <input
+          value={title}
+          onChange={(e) => setTtile(e.target.value)}
+          type="text"
+          className="text-field"
+          placeholder="Note title"
+        />
+        <input
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
           type="text"
           className="text-field"
           placeholder="Note description"
