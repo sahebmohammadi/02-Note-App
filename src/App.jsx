9dd6592d41295c618cc7477dcb78543d563b3455
Saheb Mohamadi <saheb.put92@gmail.com>
2023-08-11 19:22:58 +0330

    add useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,60 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import "./App.css";
 import AddNewNote from "./components/AddNewNote";
 import NoteList from "./components/NoteList";
 import NoteStatus from "./components/NoteStatus";
 import NoteHeader from "./components/NoteHeader";
 
+function notesReducer(notes, action) {
+  switch (action.type) {
+    case "add": {
+      return [...notes, action.payload];
+    }
+    case "delete": {
+      return notes.filter((s) => s.id !== action.payload);
+    }
+    case "complete": {
+      return notes.map((note) =>
+        note.id === action.payload
+          ? { ...note, completed: !note.completed }
+          : note
+      );
+    }
+    default:
+      throw new Error("unknown action" + action.type);
+  }
+}
+
 function App() {
-  const [notes, setNotes] = useState([]);
+  // const [notes, setNotes] = useState([]);
+  const [notes, dispatch] = useReducer(notesReducer, []);
   const [sortBy, setSortBy] = useState("latest");
 
   const hanldeAddNote = (newNote) => {
-    setNotes((prevNotes) => [...prevNotes, newNote]);
+    // setNotes((prevNotes) => [...prevNotes, newNote]);
+    dispatch({ type: "add", payload: newNote });
   };
 
   const handleDeleteNote = (id) => {
-    // const filteredNotes = notes.filter((n) => n.id !== id);
-    // setNotes(filteredNotes);
-    setNotes((prevNotes) => prevNotes.filter((n) => n.id !== id));
+    // setNotes((prevNotes) => prevNotes.filter((n) => n.id !== id));
+    dispatch({ type: "delete", payload: id });
   };
 
   const handleCompleteNote = (e) => {
     const noteId = Number(e.target.value);
+    dispatch({ type: "complete", payload: noteId });
+
     // const newNotes = notes.map((note) =>
     //   note.id === noteId ? { ...note, completed: !note.completed } : note
     // );
     // setNotes(newNotes);
-
-    setNotes((prevNotes) =>
-      prevNotes.map((note) =>
-        note.id === noteId ? { ...note, completed: !note.completed } : note
-      )
-    );
+    
+    // setNotes((prevNotes) =>
+    // prevNotes.map((note) =>
+    // note.id === noteId ? { ...note, completed: !note.completed } : note
+    // )
+    // );
   };
-
-
 
   return (
     <div className="container">
